6ee92d771369c113052bf0b607386c434a659abc
Bananadev3 <angelomodanez30@gmail.com>
2024-03-07 03:07:34 -0300

    Add HTML and Basic CSS, Created RandomPhrase Function

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -3,36 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
-  styleUrl: './calc.component.css'
+  styleUrl: './calc.component.css',
 })
 export class CalcComponent {
-
-  num1: number = 0;
-  num2: number = 0;
+  num1: any = '';
+  num2: any = '';
   total: number = 0;
+  nomes: string = '';
+  random: any;
 
-  add(){
+  add() {
     this.total = this.num1 + this.num2;
   }
 
-  sub(){
+  sub() {
     this.total = this.num1 - this.num2;
   }
 
-  div(){
+  div() {
     this.total = this.num1 / this.num2;
   }
 
-  mul(){
+  mul() {
     this.total = this.num1 * this.num2;
   }
 
-  per(){
+  per() {
     this.total = (this.num1 / this.num2) * 100;
   }
 
-  pot(){
-   this.total = Math.pow(this.num1, this.num2);
+  pot() {
+    this.total = Math.pow(this.num1, this.num2);
+  }
+
+  getRandom() {
+    this.random = Math.floor(Math.random() * 10);
+    if (this.random == 0) this.nomes = '';
+    if (this.random == 1) this.nomes = '';
+    if (this.random == 2) this.nomes = '';
+    if (this.random == 3) this.nomes = '';
+    if (this.random == 4) this.nomes = '';
+    if (this.random == 5) this.nomes = '';
+    if (this.random == 6) this.nomes = '';
+    if (this.random == 7) this.nomes = '';
+    if (this.random == 8) this.nomes = '';
+    if (this.random == 9) this.nomes = '';
   }
 }
-
